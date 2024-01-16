c9292fdd9fea27ba420922cc30ddd321dbb6032d
Kardo Kiyani <kardo1998@gmail.com>
2024-01-16 15:05:05 +0100

    Add typed.js

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(__dirname + "/node_modules/bootstrap/dist"));
-app.use(express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use(express.static(__dirname + "/node_modules/jquery/dist/"));
+app.use(express.static(__dirname + "/node_modules/typed.js/lib"));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
