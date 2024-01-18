6886221f3ef0ee0276adbe7d9917c89fb9ffeeac
Kardo Kiyani <kardo1998@gmail.com>
2024-01-18 19:38:12 +0100

    Task

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -13,23 +13,44 @@ router.get('/', function(req, res, next) {
 });
 //Post New Recommendations
 router.post('/', jsonParser, function(req, res, next) {
-    let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
-    let recommendationsArray = JSON.parse(rawdata);
-    if(recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
-      const newArray = recommendationsArray.concat([req.body])
-      fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  const expectedAttributed = ["avatar", "name", "role", "description"]
+    Object.keys(req.body).forEach(param => {
+      if (!(expectedAttributed.includes(param))) {
+        console.log(param);
+        res.status(400).end("Wrong Attr");
+      }else{
+        if(req.body[param] == ''){
+          res.status(400).end(param + " must have a value");
+        }
+      }
+    });
+    if (req.body.avatar == null || req.body.name == null) {
+      res.status(400).end("Avatar/name not provided");
     }
-    res.end();
-  });
+    if (!([1, 2, 3].includes(req.body.avatar))) {
+      res.status(400).end("Wrong avatar provided")
+    }
+    
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let recommendationsArray = JSON.parse(rawdata);
+  if(recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
+    const newArray = recommendationsArray.concat([req.body])
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  }
+  res.end();
+});
 //Delete Recommendations
-router.delete('/', jsonParser, function(req, res, next) {
-    let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
-    let recommendationsArray = JSON.parse(rawdata);
-    const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
-    if (newArray.length !== recommendationsArray.length ) {
-      fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
-    }
-    res.end();
-  });
+router.delete('/', jsonParser, ensureLogIn, function(req, res, next) {
+  if (req.body.name == null) {
+    res.status(400).end("Name not provided");
+  }
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let recommendationsArray = JSON.parse(rawdata);
+  const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
+  if (newArray.length !== recommendationsArray.length ) {
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  }
+  res.end();
+});
 
 module.exports = router;
