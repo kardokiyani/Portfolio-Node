12b3b1f646be95a26150d55e8baaeefe56a13ee4
Kardo Kiyani <kardo1998@gmail.com>
2024-01-18 18:10:16 +0100

    Portfolio refactor

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs")
+const path = require("path")
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  // Read recommendations.json
-  let recommendationsData = fs.readFileSync(
-    path.resolve(__dirname, '../data/recommendations.json')
-  );
-  let recommendations = JSON.parse(recommendationsData);
-
-  // Pass recommendations data to index.ejs view
-  res.render('index', { title: 'Express', data: recommendations });
+router.get('/', function(req, res, next) {
+  let introductionData = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let recommendationsData = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let portfolioData = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  res.render('index', { title: 'Express', array: JSON.parse(introductionData), data: JSON.parse(recommendationsData), cakes: JSON.parse(portfolioData) });
 });
 
 module.exports = router;
