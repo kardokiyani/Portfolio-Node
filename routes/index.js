e1f6e885ea2674ff34f56ad26bfac389c5bf48d3
Kardo Kiyani <kardo1998@gmail.com>
2024-01-18 13:13:59 +0100

    Refactor recommendations’

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const fs = require("fs")
-const path = require("path")
+const fs = require('fs');
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
-  res.render('index', { title: 'Express' , array: JSON.parse(data)});
+router.get('/', function (req, res, next) {
+  // Read recommendations.json
+  let recommendationsData = fs.readFileSync(
+    path.resolve(__dirname, '../data/recommendations.json')
+  );
+  let recommendations = JSON.parse(recommendationsData);
+
+  // Pass recommendations data to index.ejs view
+  res.render('index', { title: 'Express', recommendations: recommendations });
 });
 
 module.exports = router;
--- routes/recommendations.js
+++ routes/recommendations.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('recommendations');
+router.get("/", function (req, res, next) {
+  let data = fs.readFileSync(
+    path.resolve(__dirname, "../data/recommendations.json")
+  );
+  res.render("recommendations", { data: JSON.parse(data) });
 });
 
 module.exports = router;
